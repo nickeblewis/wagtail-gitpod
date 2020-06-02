4776e9de0be16f0db59cc0016b7eb9804d0d59a0
Nick Lewis <nickeblewis@gmail.com>
2020-06-02 22:21:31 +0000

    Completed tutorial

--- home/models.py
+++ home/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
 class HomePage(Page):
+    body = RichTextField(blank=True)
     banner_title = models.CharField(max_length=50, default='Hello World!')
+
     content_panels = Page.content_panels + [
-        FieldPanel("banner_title")
+        FieldPanel("banner_title"),
+        FieldPanel('body', classname='full')
     ]
